4501da348e8bf47c3bc47f18086a62303c4ffe23
noukie - <noukie@mbp-de-noukie.home>
2024-11-12 18:30:37 +0100

    modif

--- Exercice1/script.js
+++ Exercice1/script.js
@@ -4,6 +4,7 @@ let champDeTexte = document.getElementById('monChamp');
 bouton.addEventListener("click", function() {
     let texte = champDeTexte.value; // récupération du texte dans le champ
     if (texte) {
+      // console.log(texte); // affiche le texte dans la console
         document.getElementById('resultat').innerText = texte; // affiche le texte dans le paragraphe
     } else {
         alert("Veuillez entrer du texte !"); // alerte si rien n'a été entré
