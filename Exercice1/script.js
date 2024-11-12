9eb9b50db512e871cacd45fd4fafe039e9fd143f
noukie - <noukie@mbp-de-noukie.home>
2024-11-12 18:18:42 +0100

    Ajout des fichiers script.js et style.css, et modification de index.html

---
+++ Exercice1/script.js
@@ -0,0 +1,11 @@
+let bouton = document.getElementById('monBouton'); // sélection
+let champDeTexte = document.getElementById('monChamp');
+
+bouton.addEventListener("click", function() {
+    let texte = champDeTexte.value; // récupération du texte dans le champ
+    if (texte) {
+        document.getElementById('resultat').innerText = texte; // affiche le texte dans le paragraphe
+    } else {
+        alert("Veuillez entrer du texte !"); // alerte si rien n'a été entré
+    }
+});
